608686032d027cccecd7bd08330385f326f3df45
kcoro <6244044+kcoro@users.noreply.github.com>
2024-05-26 16:41:29 -0400

    Timer and email submit

---
+++ js/subscribe.js
@@ -0,0 +1,4 @@
+function subscribe() {
+  document.getElementById("subscribe-form").innerHTML =
+    '<p id="subscribed"> Done!</p><p id="subscribed-text">You have successfully subscribed. See you soon!</p>';
+}
